e3b9692a66fb873f61b28a5d87a112561aea2397
yumiQA <lulugogosing@gmail.com>
2020-02-22 10:53:44 -0500

    Triangle project careated Assignment2

---
+++ YLeeAssignment2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Yumi Lee
+Assignment: Assignment2
+This program is for checking triangle type
+Course Code: PROG2070 Software Quality Assurance
+Professor: Rob Bowyer
+Date: February 22, 2020 */
+
+namespace YLeeAssignment2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
