31aedcf9f9358506f5eb87e79cc6fd692f97b077
yumiQA <lulugogosing@gmail.com>
2020-02-23 09:16:26 -0500

    Classes created - Triagle and Analyze

---
+++ YLeeAssignment2/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Yumi Lee
+Assignment: Assignment2
+This program is for checking triangle type
+Course Code: PROG2070 Software Quality Assurance
+Professor: Rob Bowyer
+Date: February 22, 2020 */
+
+namespace YLeeAssignment2
+{
+    public class Triangle
+    {
+        private int triangleDimension;
+
+        public Triangle()
+        {
+            triangleDimension = 1;
+        }
+
+        public Triangle(int triangleDimension)
+        {
+            this.triangleDimension = triangleDimension;
+        }
+
+        public int GetTriangle()
+        {
+            return triangleDimension;
+        }
+
+        public void SetTriangle(int triangle)
+        {
+            this.triangleDimension = triangle;
+        }
+
+
+    }
+}
---
+++ YLeeAssignment2/TriangleSolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YLeeAssignment2
+{
+    public static class TriangleSolver
+    {
+        public static string Analize(int inputOne, int inputTwo, int inputThree)
+        {
+            return "";
+        }
+
+    }
+}
